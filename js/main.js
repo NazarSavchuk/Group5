74ad53c6c0ea1f737c59c60ca7f7fba10e80218c
NazarSavchuk <nazarsavcuk3@gmail.com>
2021-12-24 13:22:39 +0200

    slider added

--- js/main.js
+++ js/main.js
@@ -41,4 +41,20 @@ document.addEventListener('DOMContentLoaded', () => {
 }
 })
 
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
+  // If we need pagination
+  
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  
+});
